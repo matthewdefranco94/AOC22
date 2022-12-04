d7c594e8ca93d9a60614718f2bee00b70d404d57
Matthew DeFranco <matthewdefranco94@gmail.com>
2022-12-03 18:14:36 -0800

    Refactoring with andrew & victor

--- #AOC Day 3.py
+++ #AOC Day 3.py
@@ -31,31 +31,28 @@ print(sum(meth))
 """
 PART 1
 """
+matches = []
 firstHalves = []
 secondHalves = []
 for pc in f:
     n = list(pc)
     la = int(len(n) / 2)
     frontHalf = n[0:la]
-    backHalf = n[la:]
-    
-    firstHalves.append(frontHalf)
-    secondHalves.append(backHalf)
-
-print(firstHalves)
-
-matches = []
-for i in range(len(firstHalves)):
-    cat = list(set(firstHalves[i]).intersection(secondHalves[i]))
+    backHalf = n[la:]  
+    cat = list(set(frontHalf).intersection(backHalf))
     matches.append(cat)
+
+print(matches)
+
 
 #this can be a function
 convert = []
 for m in matches:
     for p in m:
         if (p.isupper()):
-            mum = ord(p) - 64 + 26
+            mum = ord(p) - ord("A") + 26
         else:
             mum = ord(p) - 96
         convert.append(mum)
 print(sum(convert))
+print(ord("A"))
